73ff4b0e2625fe3e9437aed8aabb3c63b7913082
Nitin Rajput <nitinrajput0093@gmail.com>
2025-09-19 13:44:05 +0530

    fix:Aligned the Folder Structure according to the requirements

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import urlRoutes from "./route/urlshortenRoutes.js";
-import { logger } from './middleware/logger.js'
+import { logger } from '../Logging Middleware/logger.js'
 
 
 
